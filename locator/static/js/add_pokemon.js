ebf32687ef00814a1141f84e102f1769023acb3a
lacunahag <jasmine.yhumbert@gmail.com>
2016-07-12 18:24:53 -0700

    and now it is broken

---
+++ locator/static/js/add_pokemon.js
@@ -0,0 +1,11 @@
+// var marker = new google.maps.Marker({
+//           position: myLatLng,
+//           map: map,
+//           title: 'Hello World!'
+//         });
+
+// google.maps.event.addListener(marker, "click", function (event) {
+//     var latitude = event.latLng.lat();
+//     var longitude = event.latLng.lng();
+//     console.log( latitude + ', ' + longitude );
+// }); //end addListener
--- locator/static/locator/js/initialize_map.js
+++ locator/static/locator/js/initialize_map.js
@@ -1,3 +1,97 @@
+var newMap = function(lat_num, lon_num, zoom_num) {
+    var map = new google.maps.Map(document.getElementById('map'), {
+        center: {
+            lat: lat_num,
+            lng: lon_num
+        },
+        zoom: zoom_num,
+    });
+    map.setOptions({
+        disableDoubleClickZoom: true
+    });
+}
+
+
+var map;
+// why does this not work OH ok lol nvm wtf
+function initMap() {
+    // Try HTML5 geolocation.
+    if (navigator.geolocation) {
+      var latitude;
+      var longitude;
+        navigator.geolocation.getCurrentPosition(function(position) {
+            latitude = position.coords.latitude;
+            longitude = position.coords.longitude;
+        });
+        global map = newMap(latitude, longitude, 14);
+    } else {
+        global map = newMap(0, 0, 3);
+    }
+
+    var infowindow = new google.maps.InfoWindow({
+        content: "yes"
+    });
+
+    google.maps.event.addListener(map, "dblclick", function(event) {
+        latitude = event.latLng.lat();
+        longitude = event.latLng.lng();
+        var marker = new google.maps.Marker({
+            position: {
+                lat: latitude,
+                lng: longitude
+            },
+            map: map,
+            title: 'New'
+        });
+        infowindow.open(map, marker);
+        console.log(latitude + ', ' + longitude);
+    }); //end addListener
+}
+
+
+
+/*
+function initMap() {
+    // Try HTML5 geolocation.
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(function(position) {
+            newMap(position.coords.latitude, position.coords.longitude, 14);
+        });
+    } else {
+        newMap(0, 0, 3);
+    }
+
+    var contentString =
+        '<div class="dropdown">' +
+        '<button class="btn btn-primary dropdown-toggle" type="button" data-toggle="dropdown">Dropdown Example' +
+        '<span class="caret"></span></button>' +
+        '<ul class="dropdown-menu">' +
+        '  <li><a href="#">HTML</a></li>' +
+        '  <li><a href="#">CSS</a></li>' +
+        '  <li><a href="#">JavaScript</a></li>' +
+        '</ul>' +
+        '</div>' +
+        '</div>';
+
+    var infowindow = new google.maps.InfoWindow({
+        content: contentString
+    });
+
+    google.maps.event.addListener(map, "dblclick", function(event) {
+        latitude = event.latLng.lat();
+        longitude = event.latLng.lng();
+        var marker = new google.maps.Marker({
+            position: event.latLng,
+            map: map,
+            title: 'New'
+        });
+        infowindow.open(map, marker);
+        console.log(latitude + ', ' + longitude);
+    }); //end addListener
+}
+*/
+
+/*
 function initMap() {
     var map = new google.maps.Map(document.getElementById('map'), {
         center: {
@@ -5,7 +99,39 @@ function initMap() {
             lng: 0
         },
         zoom: 3
+    }); // end standard map initialize
+
+    map.setOptions({
+        disableDoubleClickZoom: true
     });
+
+    var contentString =
+    '<div class="dropdown">'+
+    '<button class="btn btn-primary dropdown-toggle" type="button" data-toggle="dropdown">Dropdown Example'+
+    '<span class="caret"></span></button>'+
+    '<ul class="dropdown-menu">'+
+    '  <li><a href="#">HTML</a></li>'+
+    '  <li><a href="#">CSS</a></li>'+
+    '  <li><a href="#">JavaScript</a></li>'+
+    '</ul>'+
+  '</div>'+
+'</div>';
+
+    var infowindow = new google.maps.InfoWindow({
+        content: contentString
+      });
+
+    google.maps.event.addListener(map, "dblclick", function(event) {
+        latitude = event.latLng.lat();
+        longitude = event.latLng.lng();
+        var marker = new google.maps.Marker({
+            position: event.latLng,
+            map: map,
+            title: 'New'
+          });
+        infowindow.open(map, marker);
+        console.log(latitude + ', ' + longitude);
+    }); //end addListener
 
     // Try HTML5 geolocation.
     if (navigator.geolocation) {
@@ -20,3 +146,14 @@ function initMap() {
         });
     }
 }
+*/
+
+// function addPokemon() {
+//     var latitude = 0.0;
+//     var longitude = 0.0;
+//     var listener = New google.maps.event.addListener(map, "dblclick", function(event) {
+//         latitude = event.latLng.lat();
+//         longitude = event.latLng.lng();
+//         console.log(latitude + ', ' + longitude);
+//     }); //end addListener
+// }
