b53de9e767876e1361d1225dc8ba1bf73069898a
lacunahag <jasmine.yhumbert@gmail.com>
2016-08-01 13:58:51 -0700

    first push

--- locator/models.py
+++ locator/models.py
@@ -5,19 +5,7 @@ from django.db import models
 import datetime
 from django.core.validators import MaxValueValidator, MinValueValidator
 
-# from oauth2client.contrib.django_orm import FlowField, CredentialsField
-
 import PIL
-
-# Create your models here.
-
-# class FlowModel(models.Model):
-#   id = models.ForeignKey(User, primary_key=True)
-#   flow = FlowField()
-#
-# class CredentialsModel(models.Model):
-#   id = models.ForeignKey(User, primary_key=True)
-#   credential = CredentialsField()
 
 class Pokemon(models.Model):
     name = models.CharField(max_length=255, blank=False, unique=True)
@@ -26,14 +14,6 @@ class Pokemon(models.Model):
     evolution = models.PositiveIntegerField()
     def __str__(self):
         return self.name
-
-""" p sure I'm just gonna use disqus
-class Comment(models.Model):
-    title = models.CharField(max_length=50)
-    text = models.TextField()
-    likes = models.IntegerField(default=0)
-    author = models.ForeignKey(User)
-"""
 
 class MapPoint(models.Model):
     pokemon = models.ForeignKey(Pokemon)
